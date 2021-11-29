c696246fc888cd82bcf62bed1a4fba288d6336e3
cosmii02 <tuisk2005@gmail.com>
2021-11-29 15:25:32 +0200

    meanings of int and string

---
+++ helloworld.py
@@ -0,0 +1,51 @@
+# Kommentaar jee
+n = 7
+a = 3*n
+#print(a)
+
+# mida iganes = 7
+a = 3*n
+#print(n+8)
+
+#print("Hello world")
+
+a = 5
+b = 3
+a = 2
+if (a < b) :
+    print (a)
+else:
+    print(b)
+# a on väiksem kui b seega prinditakse a, ning a väärtus on 2
+
+a = 5
+b = 3
+a = 2
+if (a > b) :
+    print (a)
+else:
+    print(b)
+# a ei ole suurem kui b seega kasutatakse funktsiooni "else" mis prindib alati tolle variandi kui if on false
+
+a = 5
+b = 3
+a = 2
+if (a == b) :
+    print (a)
+else:
+    print(a+b)
+# a ei ole sama mis b seega kasutatakse else funktsiooni, ning see prindib a+b vastuse
+
+a = 5
+b = 3
+a = 2
+if (a < b) :
+    print (a*4)
+else:
+    print(b)
+# a on väiksem kui b seega prinditakse esimene if variant, ning too prindib a*4 vastuse
+
+'''
+int() on täisarvude infokogum
+string on jutt või sõna (tekstiandme tüüp)
+'''
