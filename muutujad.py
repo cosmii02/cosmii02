99e8cfbd0559619b4154df21fd562ca294fa995b
cosmii02 <tuisk2005@gmail.com>
2021-12-01 18:09:36 +0200

    uwu

--- muutujad.py
+++ muutujad.py
@@ -1,8 +1,9 @@
 from math import pi
-
+'''
 print('Tere, kenneth')
 # Prindib tervituse kennethile
 
+#ul 2
 tehe=(5+2)*8/7+3
 print(tehe)
 # Teeb matemaatilise tehte ja prindib selle
@@ -16,3 +17,14 @@ r=c/(2*pi)
 d=r*2
 print(d)
 # Arvutab puu labimoodu
+'''
+
+##########################################################################
+#ul 1.2
+aasta='2016. '
+president='Kersti Kaljulaid'
+lause_keskosa='Aastal valiti Eesti presidendiks '
+lause=(aasta+lause_keskosa+president)
+print(lause)
+
+#ul 1.3
