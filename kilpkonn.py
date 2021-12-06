7646e24dfbb747719f898dee2c9723f4c2697e6b
cosmii02 <tuisk2005@gmail.com>
2021-12-06 10:21:28 +0200

    turtle but no color quite yet

---
+++ kilpkonn.py
@@ -0,0 +1,11 @@
+from turtle import*
+import turtle
+  
+turtle.colormode(255)
+t = turtle.Turtle()
+ 
+s = int(input("Enter the length of the side of square: "))
+for _ in range(4):
+  t.forward(s) # Forward turtle by s units
+  t.left(90) # Turn turtle by 90 degree
+exitonclick()
