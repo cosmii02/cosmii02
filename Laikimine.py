e72d5a1194f424089964095b0c01b7401e24e7d0
Cosmii02 <tuisk2005@gmail.com>
2022-01-25 14:42:10 +0100

    ohjah

--- Laikimine.py
+++ Laikimine.py
@@ -2,6 +2,6 @@ minut=int(input("Minutite arv: "))
 like=1
 while like <= 6:
     minut = minut + like
-    
+
     like +=2
-print("Laikide koguarv on ", minut)
+print("Laikide koguarv on ", minut) 
