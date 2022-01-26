ed87cd6719d96e0b633ece310fec60acbc7142dd
Torren <torren.tamm@gmail.com>
2022-01-26 11:14:10 +0200

    Parandus

--- 3-2_Laikimine.py
+++ 3-2_Laikimine.py
@@ -6,4 +6,4 @@ while i <= minutid: # Teeb nii kaua, kui on veel minuteid, mida arvestada
     if i % 2: # Kui i, ehk arvestatav minut, on paarisarv
         laigid += i # Lisab laikidele arvestatava minuti
     i += 1
-print("Laikide koguarv on " + str(likes)) # Väljastab laikide arvu
+print("Laikide koguarv on " + str(laigid)) # Väljastab laikide arvu
