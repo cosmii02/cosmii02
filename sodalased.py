9a44c2683bb72901e481a99ee32be029a871e7cf
Cosmii02 <tuisk2005@gmail.com>
2022-02-17 11:11:19 +0200

    sodalased.py completed successfully

--- sodalased.py
+++ sodalased.py
@@ -1,14 +1,27 @@
 import random
 import click
-sodalane1=1
-sodalane2=2
-isdamage= random.randint(0,1)
-if isdamage==1:
-    sodalane2-20
-    print ("Sõdalane 1 ründas teist sõdalast ja sõdalane 2 kaotas 20hp")
-    print ("Sõdalane 1 elud: 100 \nSõdalane 2 elud: 80")
-elif isdamage==0:
-    sodalane1-20
-    print ("case2")
-if click.confirm('Do you want to continue?', default=True):
-    rdmg
+
+sodalane1 = 100
+sodalane2 = 100
+while True:
+    isdamage= random.randint(0,1)
+    if isdamage==1:
+        sodalane2=sodalane2-20
+        print ("Sõdalane 1 ründas teist sõdalast ja sõdalane 2 kaotas 20hp")
+        print ("Sõdalane 1 elud: "+(str(sodalane1)))
+        print ("Sõdalane 2 elud: " + (str(sodalane2)))
+    elif isdamage==0:
+        sodalane1=sodalane1-20
+        print("Sõdalane 2 ründas teist sõdalast ja sõdalane 1 kaotas 20hp")
+        print ("Sõdalane 1 elud: " + (str(sodalane1)))
+        print ("Sõdalane 2 elud: "+(str(sodalane2)))
+    if sodalane2<=0:
+        print("\nMäng läbi, sõdalane 2 suri")
+        break
+    if sodalane1<=0:
+        print("\nMäng läbi, sõdalane 1 suri")
+        break
+    if click.confirm('Kas tahad jätkata?', default=True):
+        continue
+    else:
+        break
