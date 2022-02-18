deb0dfed4c19e07d1895d575ac80b447ead6c761
Cosmii02 <tuisk2005@gmail.com>
2022-02-18 10:41:54 +0200

    stuff and stuff

--- lottery.py
+++ lottery.py
@@ -1,118 +1,131 @@
 import random
 
-random_draw_list = random.sample(range(1, 76), 75)
 
-def generate_card():
-    """
-    Generates a bingo card and stores the numbers in a dictionary.
-    """
-    card = {
-        "B": [],
-        "I": [],
-        "N": [],
-        "G": [],
-        "O": [],
-    }
-    min = 1
-    max = 15
-    for letter in card:
-        card[letter] = random.sample(range(min, max), 5)
-        min += 15
-        max += 15
-        if letter == "N":
-            card[letter][2] = "X" # free space!
-    return card
+class Board:
+    def __init__(self):
+        self.position = {}
+        self.playBoard = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        self.bingo = {
+            "row": [0, 0, 0, 0, 0],
+            "col": [0, 0, 0, 0, 0],
+            "diagonal": [0, 0]
+        }
 
-def print_card(card):
-    """
-    Prints the bingo card.
+        self.createBoard()
 
-    Arguments:
-        card (dictionary): The card to be printed out.
-    """
-    for letter in card:
-        print(letter, end="\t")
-        for number in card[letter]:
-            print(number, end="\t")
-        print("\n")
-    print("\n")
+    def createBoard(self):
+        choices = [i for i in range(1, 26)]
+        for i in range(5):
+            for j in range(5):
+                choice = random.choice(choices)
+                self.playBoard[i][j] = choice
+                choices.pop(choices.index(choice))
+                self.position[choice] = (i, j)
 
-def draw(card, list):
-    """
-    Pops a number off a list of random numbers. Using the pop method ensures no duplicate
-    numbers will be drawn.
+    def updateBoard(self, val):
+        x, y = self.position[val]
+        self.playBoard[x][y] = 'X'
+        self.updateBingo(x, y)
 
-    Arguments:
-        card (dictionary): The card to to check for the number that was drawn.
-        list (list): The list of random numbers to be drawn from.
-    """
-    number_drawn = random_draw_list.pop()
-    for letter in card:
-        i = 0
-        for number in card[letter]:
-            if number == number_drawn:
-                card[letter][i] = "X"
-            i += 1
-    return number_drawn
+    def updateBingo(self, x, y):
+        self.bingo["row"][x] += 1
+        self.bingo["col"][y] += 1
+        if x == y == 2:
+            self.bingo["diagonal"][0] += 1
+            self.bingo["diagonal"][1] += 1
+        elif x == y:
+            self.bingo["diagonal"][0] += 1
+        elif x + y == 4:
+            self.bingo["diagonal"][1] += 1
 
-def check_win(card):
-    """
-    First checks for diagonal wins, then four-corner, then horizontal, and finally, vertical.
+    def checkBingo(self):
+        return 5 in self.bingo["row"] or 5 in self.bingo["col"] or 5 in self.bingo["diagonal"]
 
-    Arguments:
-        card (dictionary): The card to check for a win.
-    """
-    win = False
-    if card["B"][0] == "X" and card["I"][1] == "X" and card["N"][2] == "X" and card["G"][3] == "X" and card["O"][4] == "X":
-        win = True
-    elif card["O"][0] == "X" and card["G"][1] == "X" and card["N"][2] == "X" and card["I"][3] == "X" and card["B"][4] == "X":
-        win = True
-    elif card["B"][0] == "X" and card["O"][4] == "X" and card["B"][4] == "X" and card["O"][0] == "X":
-        win = True
-    for letter in card:
-        if(len(set(card[letter]))==1):
-            win = True
-    for i in range(5):
-        cnt = 0
-        for letter in card:
-            if card[letter][i] == "X":
-                cnt += 1
-        print(cnt)
-        if cnt == 5:
-            win = True
-            break
-    return win
 
-def main():
-    """
-    Main method for the program. Generates a card, prints it, and loops through drawing
-    and printing until the check_win method returns True or the user enters "quit".
-    """
-    print("Let's play bingo!")
-    card = generate_card()
+class Player(Board):
+    def __init__(self, name):
+        self.name = name
+        self.board = Board()
 
-    print("\nHere is your card:\n")
-    print_card(card)
+    def updatePlayerBoard(self, val):
+        self.board.updateBoard(val)
 
-    print("\nKeep pressing enter to continue or type quit to exit.\n")
-    user_input = input()
-    win = check_win(card)
-    balls_till_win = 0
+    def checkBingo(self):
+        return self.board.checkBingo()
 
-    while win == False and user_input != "quit":
-        number_drawn = draw(card, random_draw_list)
-        balls_till_win += 1
+class Game:
+    def displayBoard(self, player1, player2):
+        board1 = player1.board.playBoard
+        board2 = player2.board.playBoard
+        size = 20
+        p1len = len(player1.name)
+        print(player1.name+" "*(size-p1len+1)+player2.name)
+        for i in range(5):
+            for j in board1[i]:
+                if j=='X':
+                    print(f" {j}",end=" ")
+                elif j>9:
+                    print(j,end=" ")
+                else:
+                    print(f"0{j}",end=" ")
+            print("      ",end="")
+            for j in board2[i]:
+                if j=='X':
+                    print(f" {j}",end=" ")
+                elif j>9:
+                    print(j,end=" ")
+                else:
+                    print(f"0{j}",end=" ")
+            print()
+        print()
 
-        print(f"\nNumber drawn: {number_drawn}.")
-        print(f"Total balls drawn: {balls_till_win}.\n")
-        print_card(card)
+game = Game()
+player1 = Player(name="player1")
+player2 = Player(name="player2")
 
-        win = check_win(card)
+game.displayBoard(player1, player2)
 
-        user_input = input()
-    if win == True:
-        print(f"\nBingo! Total balls to win: {balls_till_win}.")
-    else:
-        print("Goodbye! Thanks for playing!")
+while True:
+    val = int(input(f"{player1.name}'s turn : "))
+    player1.updatePlayerBoard(val)
+    player2.updatePlayerBoard(val)
+    game.displayBoard(player1, player2)
 
-main()
+    if player1.checkBingo() and player2.checkBingo():
+        print("DRAW")
+        break
+    if player1.checkBingo():
+        player1_1=0
+        player1_1+=1
+        print(f"{player1.name} WON {player1_1} times")
+        break
+    if player2.checkBingo():
+        player2_2=0
+        player2_2+=1
+        print(f"{player2.name} WON {player2_2} times")
+        break
+
+    val = int(input(f"{player1.name}'s turn : "))
+    player1.updatePlayerBoard(val)
+    player2.updatePlayerBoard(val)
+    game.displayBoard(player1, player2)
+
+    if player1.checkBingo() and player2.checkBingo():
+        print("DRAW")
+        break
+    if player1.checkBingo():
+        player1_1=0
+        player1_1+=1
+        print(f"{player1.name} WON {player1_1} times")
+        break
+    if player2.checkBingo():
+        player2_2=0
+        player2_2+=1
+        print(f"{player2.name} WON {player2_2} times")
+        break
