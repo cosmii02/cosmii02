ee2e91b53a28cce9505bb0c53d7aec92700d8523
Cosmii02 <tuisk2005@gmail.com>
2022-03-03 14:34:37 +0200

    stuff and stuff

---
+++ e-ope/regulaar_avaldised.py
@@ -0,0 +1,21 @@
+# Vastavuse kontroll
+# import re
+# if re.match ("mati", "mati"):  print ("sobib")
+# if re.match(".mati", "umati"):  print ("sobib")
+# if re.match(".*mati", "unemati"):  print ("sobib")
+
+
+#################
+# Arvude otsimine lausest
+#################
+# import re
+# m=re.findall('\d+', 'Karja 20 lehma ja 10 mullikat')
+# for arv in m:
+#     print (arv)
+
+
+#################
+# Asendamine
+#################
+# import re
+# print (re.sub('\d+', 'x', 'Karja 20 lehma ja 10 mullikat'))
