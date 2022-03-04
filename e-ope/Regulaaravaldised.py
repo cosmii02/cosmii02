19064375b70f39933ca41e8e73d2c9e25e0acf6d
Cosmii02 <tuisk2005@gmail.com>
2022-03-04 20:09:31 +0200

    Pangandus 0% done but rest should be fine

---
+++ e-ope/Regulaaravaldised.py
@@ -0,0 +1,21 @@
+import re
+
+def ip0s():
+    fh = open('lorem.txt')
+    for line in fh:
+        if re.search('[.]', line):
+             print(line,end='')
+def passes():
+    fh = open('lorem.txt')
+    for line in fh:
+        if re.search('[A-Z]', line):
+            if re.search('[0-9]', line):
+                if re.search('[a-zA-Z]', line):
+                    print(line, end='')
+userask=str(input("1. IP aadressid\n2. Paroolid\n"))
+if userask=="1" or userask=="1.":
+    ip0s()
+elif userask=="2"or userask=="2.":
+    passes()
+else:
+    print("You can only enter '1' or '2'")
