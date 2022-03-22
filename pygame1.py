3cb0c9fe15d19a1e39aa44576c0efc5411f83a6f
Cosmii02 <tuisk2005@gmail.com>
2022-03-22 11:24:31 +0200

    Resort and more

---
+++ pygame1.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([300,300])
+pygame.display.set_caption("Lumemees - Kenneth ja Kaup")
+
+#colors
+
+#joonistus
+
+pygame.draw.circle(screen,[255,255,255] [150,200],40,100)
+pygame.draw.circle(screen,[255,255,255] [150,135],30,100)
+pygame.draw.circle(screen,[255,255,255] [150,88],20,100)
+
+pygame.display.flip()
