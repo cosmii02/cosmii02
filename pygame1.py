356770dc01117642fbac68cbaba29b4addbebbe3
Cosmii02 <tuisk2005@gmail.com>
2022-03-22 12:41:09 +0200

    more stuff

--- pygame1.py
+++ pygame1.py
@@ -8,8 +8,11 @@ pygame.display.set_caption("Lumemees - Kenneth ja Kaup")
 
 #joonistus
 
-pygame.draw.circle(screen,[255,255,255] [150,200],40,100)
-pygame.draw.circle(screen,[255,255,255] [150,135],30,100)
-pygame.draw.circle(screen,[255,255,255] [150,88],20,100)
+pygame.draw.circle(screen,[255,255,255], [150,200],40,100)
+pygame.draw.circle(screen,[255,255,255], [150,135],30,100)
+pygame.draw.circle(screen,[255,255,255], [150,88],20,100)
+
+#lumememme nina
+pygame.draw.polygon(screen, [255,153,0], [[147,90],[151,90],[150,100]],3)
 
 pygame.display.flip()
