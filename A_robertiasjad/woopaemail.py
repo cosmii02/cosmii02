7d05d1a7fd17659a30f350444f789087c0680a5b
cosmii02 <cosmii02@cosmii02.com>
2022-06-13 15:17:59 +0300

    UwU

--- A_robertiasjad/woopaemail.py
+++ A_robertiasjad/woopaemail.py
@@ -1,11 +1,13 @@
 import smtplib # Impordib smtplib mooduli
 
-sender_email = "12northolt@gmail.com" # Saatja e-maili aadress
-rec_email = "johangodinho69@gmail.com" # küsib kasutajalt saaja e-maili
+sender_email = "vikk.opilane@outlook.com" # Saatja e-maili aadress
+rec_email = "cosmii02@cosmii02.com" # küsib kasutajalt saaja e-maili
 password = input(str("Please enter your password : ")) # küsib sisestatud parooli
-message = "Hey, this was sent using python" # sõnum, mida saadetakse
+SUBJECT = input(str("Please enter subject : ")) # küsib sisestatud teema
+TEXT = input(str("Please enter body : ")) # küsib sisestatud sisu
+message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT) # määrab sõnumi
 
-server = smtplib.SMTP('smtp.gmail.com', 587) # Sisestab sisestatud e-maili
+server = smtplib.SMTP('smtp-mail.outlook.com', 587) # Sisestab sisestatud e-maili
 server.starttls() # alustab starttls protokolli
 server.login(sender_email, password) # Sisestab sisestatud e-maili
 print("Login success") # Annab teada et sisselogimine õnnestus
