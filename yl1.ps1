daae97b2ad5818e62ef26991592de9c168d8479a
cosmii02 <cosmii02@cosmii02.com>
2024-01-06 16:28:58 +0000

    Cleanup of trash

--- yl1.ps1
+++
@@ -1,8 +0,0 @@
-# Tuvastame PowerShelli versiooni ja salvestame tulemused tekstifaili
-Get-Host > "$env:userprofile\Desktop\Ülesanne01.txt"
-
-# Tuvastame ketaste info ja salvestame tulemused tekstifaili lõppu
-Get-Disk >> "$env:userprofile\Desktop\Ülesanne01.txt"
-
-# Väljastame faili sisu, et veenduda, et nii versioon kui ketaste info on olemas
-Get-Content "$env:userprofile\Desktop\Ülesanne01.txt"
